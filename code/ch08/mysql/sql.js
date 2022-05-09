68a7e22a5d03eba9af802bbc9da2c037a2236695
ding-co <pangoons@naver.com>
2022-05-10 07:21:06 +0900

    [ADD] ch 08 - part 2

--- code/ch08/mysql/sql.js
+++ code/ch08/mysql/sql.js
@@ -1,3 +1,6 @@
 module.exports = {
   customerList: `select * from customer;`,
+  customerInsert: `insert into customer set ?`,
+  customerUpdate: `update customer set ? where customer_id=?`,
+  customerDelete: `delete from customer where customer_id=?`,
 };
